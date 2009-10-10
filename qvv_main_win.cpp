fe60cef22c80bb18fa5034ba1d88f1c23e6e0644
Vladi Belperchinov-Shabanski <cade@datamax.bg>
2009-10-11 02:34:47 +0300

    maint

---
+++ qvv_main_win.cpp
@@ -0,0 +1,486 @@
+#include <QApplication>
+#include <QAction>
+#include <QLayout>
+#include <QMenu>
+#include <QMenuBar>
+#include <QStatusBar>
+#include <QTextEdit>
+#include <QFile>
+#include <QDataStream>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QSignalMapper>
+#include <QPainter>
+#include <QMouseEvent>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QLabel>
+#include <QDateTime>
+#include <QProgressDialog>
+
+#include <QTreeWidgetItem>
+
+#include <qdebug.h>
+
+#include <qvv.h>
+
+QString extensions_filter( ".JPG.JPEG.PNG.GIF.BMP.XPM." );
+
+/*****************************************************************************/
+
+QvvMainWindow::QvvMainWindow()
+    : QMainWindow()
+{
+    opt_thumbs = 0;
+    opt_dirs_only = 0;
+
+    setObjectName( "QvvMainWindow" );
+    setWindowTitle( "QVV4" );
+
+    setWindowIcon( QIcon( ":/images/Half_Face_by_uzorpatorica.jpg" ) );
+
+    resize( 640, 400 );
+
+    tree = new QTreeWidget( this );
+    tree->setMinimumSize( 400, 200 );
+
+    QTreeWidgetItem *header = new QTreeWidgetItem();
+    header->setText( 0, "Type" );
+    header->setText( 1, "Name" );
+    header->setText( 2, "Size" );
+    header->setText( 3, "Modify Time" );
+    header->setTextAlignment( 2, Qt::AlignRight );
+
+    tree->setColumnCount( 4 );
+    tree->setHeaderItem( header );
+
+    tree->setColumnWidth( 0,  70 );
+    tree->setColumnWidth( 1, 300 );
+    tree->setColumnWidth( 2,  80 );
+    tree->setColumnWidth( 3, 120 );
+
+    tree->setIconSize( QSize( 64, 64 ) );
+
+    tree->setSortingEnabled( 1 );
+    tree->sortByColumn( 1, Qt::AscendingOrder );
+
+    tree->setDragEnabled( 1 );
+
+    setCentralWidget( tree );
+
+    setupMenuBar();
+
+    statusBar()->showMessage( "." );
+
+    loadDir( QString( "." ) );
+
+}
+
+/*****************************************************************************/
+
+void QvvMainWindow::loadDir( QString path )
+{
+  QString last_path = cdir.absolutePath();
+
+  cdir.cd( path );
+
+  QString new_path = cdir.absolutePath();
+
+  setWindowTitle( "QVV4: " + new_path );
+
+  QStringList filters;
+  filters.append( QString( "*" ) );
+
+  QFileInfoList info_list = cdir.entryInfoList( filters );
+
+  QTreeWidgetItem *current = NULL;
+
+  tree->clear();
+  for( int i = 0; i < info_list.count(); i++ )
+    {
+    QFileInfo fi = info_list.at( i );
+
+    if( opt_dirs_only && ! fi.isDir() ) continue;
+
+    if( fi.fileName() == "."  ) continue;
+    if( fi.fileName() == ".." ) continue;
+
+    QString ext = "." + fi.suffix() + ".";
+    if( ! fi.isDir() && extensions_filter.indexOf( ext.toUpper() ) < 0 ) continue;
+
+    QString file_name = fi.fileName();
+
+    QTreeWidgetItem *item = new QTreeWidgetItem();
+    item->setText( 0, fi.isDir() ? ITEM_TYPE_DIR : "" );
+    item->setText( 1, file_name );
+    item->setText( 2, QVariant( fi.size() ).toString() );
+    item->setText( 3, fi.lastModified().toString( "yyyy-MM-dd hh:mm:ss" ) );
+    item->setTextAlignment( 2, Qt::AlignRight );
+
+    if( last_path == new_path + "/" + file_name ) current = item;
+
+    tree->addTopLevelItem( item );
+    }
+
+  if( current )
+    tree->setCurrentItem( current );
+  else
+    tree->setCurrentItem( tree->topLevelItem( 0 ) );
+
+  statusBar()->showMessage( QString( "Loaded items: " ) + QVariant( tree->topLevelItemCount() ).toString() );
+
+  if( opt_thumbs )
+    loadThumbs();
+};
+
+/*****************************************************************************/
+
+void QvvMainWindow::loadThumbs()
+{
+  QProgressDialog pd( "Loading thumbnails...", "Cancel", 0, tree->topLevelItemCount() - 1 );
+
+  QString new_path = cdir.absolutePath();
+
+  for( int i = 0; i < tree->topLevelItemCount(); i++ )
+    {
+    pd.setValue( i );
+    if ( pd.wasCanceled() )
+      break;
+
+    QTreeWidgetItem *item = tree->topLevelItem( i );
+    if( item->text( 0 ) == ITEM_TYPE_DIR ) continue;
+
+    QIcon icon( new_path + "/.thumbnails/" + item->text( 1 ) );
+    if( ! icon.isNull() ) item->setIcon( 1, icon );
+    }
+
+}
+
+/*****************************************************************************/
+
+void QvvMainWindow::goToDir( int mode )
+{
+  mode = ! mode; // FIXME: for what??
+  QFileDialog::Options options = QFileDialog::DontResolveSymlinks | QFileDialog::ShowDirsOnly;
+/*
+  if (!native->isChecked())
+      options |= QFileDialog::DontUseNativeDialog;
+*/
+  QString new_dir = QFileDialog::getExistingDirectory( this,
+                              tr( "Change directory" ),
+                              cdir.absolutePath(),
+                              options );
+  if ( ! new_dir.isEmpty() )
+      loadDir( new_dir );
+};
+
+/*****************************************************************************/
+
+void QvvMainWindow::enter( QTreeWidgetItem *item )
+{
+  if( item->text( 0 ) == ITEM_TYPE_DIR )
+    {
+    loadDir( item->text( 1 ) );
+    }
+  else
+    {
+    QvvView *view;
+    if( views.count() > 0 )
+      {
+      view = views[0];
+      }
+    else
+      {
+      view = new QvvView( this );
+      views.append( view );
+      }
+    view->load( cdir.absolutePath() + "/" + item->text( 1 ) );
+    }
+};
+
+/*****************************************************************************/
+
+void QvvMainWindow::closeView( QvvView *view )
+{
+  view->close();
+  views.removeOne( view );
+  delete view;
+};
+
+/*****************************************************************************/
+
+void QvvMainWindow::slotNewWindow()
+{
+  QvvMainWindow *new_mainwin = new QvvMainWindow();
+  new_mainwin->resize(600, 400);
+  new_mainwin->show();
+};
+
+/*****************************************************************************/
+
+void QvvMainWindow::slotGoUp()
+{
+  loadDir( ".." );
+};
+
+/*****************************************************************************/
+
+void QvvMainWindow::slotGoPrev()
+{
+  goPrevNext( -1 );
+};
+
+void QvvMainWindow::slotGoNext()
+{
+  goPrevNext( +1 );
+}
+
+void QvvMainWindow::goPrevNext( int r )
+{
+  QTreeWidgetItem *lwi = tree->currentItem();
+
+  int i;
+  int x = tree->topLevelItemCount();
+
+  i = tree->indexOfTopLevelItem( lwi );
+
+  if( x == 0 or i < 0 ) return; // not found or empty list
+
+  while(4)
+    {
+    i += r;
+    lwi = NULL;
+    if( i >= x or i < 0 ) break;
+    lwi = tree->topLevelItem( i );
+    if( lwi->text( 0 ) == ITEM_TYPE_DIR ) continue;
+    break;
+    }
+  qDebug() << i;
+  if( lwi )
+    {
+    tree->setCurrentItem( lwi );
+    enter( lwi );
+    }
+};
+
+/*****************************************************************************/
+
+void QvvMainWindow::slotThumbs()
+{
+  opt_thumbs = ! opt_thumbs;
+  if( opt_thumbs ) loadThumbs();
+
+  statusBar()->showMessage( opt_thumbs ? tr( "Thumbnails enabled" ) : tr( "Thumbnails disabled" ) );
+};
+
+void QvvMainWindow::slotChangeDir()
+{
+  goToDir( 0 );
+}
+
+void QvvMainWindow::slotHomeDir()
+{
+  loadDir( cdir.homePath() );
+}
+
+void QvvMainWindow::slotReloadDir()
+{
+  loadDir( "." );
+}
+
+void QvvMainWindow::slotShowDirsOnly()
+{
+  opt_dirs_only = ! opt_dirs_only;
+  loadThumbs();
+
+  statusBar()->showMessage( opt_thumbs ? tr( "Show directories only" ) : tr( "Show all directories and files" ) );
+}
+
+/*****************************************************************************/
+
+void QvvMainWindow::keyPressEvent ( QKeyEvent * e )
+{
+
+  if( e->modifiers() & Qt::ALT )
+    {
+    switch( e->key() )
+      {
+      case Qt::Key_X   : QApplication::quit(); break;
+      default: QMainWindow::keyPressEvent( e );
+      }
+    }
+  else
+    {
+    switch( e->key() )
+      {
+/*
+      case Qt::Key_F3    : slotNewWindow(); break;
+      case Qt::Key_F4    : close();
+                           delete this;
+                           break;
+*/
+      case Qt::Key_Left  : slotGoUp(); break;
+      case Qt::Key_Right :
+      case Qt::Key_Return: enter( tree->currentItem() ); break;
+
+      //case Qt::Key_F6    : slotThumbs(); break;
+
+
+/*
+      case Qt::Key_F1    : closeAll();
+                           views.append( new qvvView( this ) );
+                           views.first()->load( "*logo*" );
+                           break;
+
+      case Qt::Key_F5    : loadDir( cdir.absolutePath() ); break;
+
+      case Qt::Key_BracketLeft  : slotGoPrev(); break;
+      case Qt::Key_BracketRight : slotGoNext(); break;
+      case Qt::Key_Backspace    :
+      case Qt::Key_Escape       : closeAll(); break;
+      case Qt::Key_Insert       : optCenter = !optCenter; break;
+      case Qt::Key_Delete       : slotDelete(); break;
+*/
+/*
+      default:
+        if ( e->text().toAscii().at(0) && isalnum(e->text().toAscii().at(0)) )
+          find( e->text().toAscii().at(0) );
+        else
+          switch( e->text().toAscii().at(0) )
+            {
+            case 13  : Enter( tree->currentItem() ); break;
+            case '~' : goToDir( '~' ); break;
+            case '`' : goToDir( '`' ); break;
+            default: QMainWindow::keyPressEvent( e );
+            }
+*/
+      default: QMainWindow::keyPressEvent( e );
+      }
+    }
+
+
+}
+
+/*****************************************************************************/
+
+void QvvMainWindow::actionTriggered(QAction *action)
+{
+    qDebug("action '%s' triggered", action->text().toLocal8Bit().data());
+}
+
+/*****************************************************************************/
+
+void QvvMainWindow::setupMenuBar()
+{
+    /*--------------------------------------------------------------------*/
+
+    QMenu *menu = menuBar()->addMenu( tr("&File") );
+
+    QAction *action;
+/*
+    action = menu->addAction(tr("Save layout..."));
+    connect(action, SIGNAL(triggered()), this, SLOT(saveLayout()));
+
+    action = menu->addAction(tr("Load layout..."));
+    connect(action, SIGNAL(triggered()), this, SLOT(loadLayout()));
+    action = menu->addAction(tr("Switch layout direction"));
+    connect(action, SIGNAL(triggered()), this, SLOT(switchLayoutDirection()));
+
+    menu->addSeparator();
+*/
+
+    action = menu->addAction( tr("&Reload directory"), this, SLOT(slotReloadDir()), Qt::Key_F5 );
+    action->setIcon( QIcon( ":/images/view-refresh.png" ) );
+
+    action = menu->addAction( tr("&Quit"), this, SLOT(close()) );
+    action->setIcon( QIcon( ":/images/system-log-out.png" ) );
+
+    /*--------------------------------------------------------------------*/
+
+    menu = menuBar()->addMenu( tr("&View"));
+
+    action = menu->addAction( tr("Enable &thumbnails") );
+    action->setCheckable( true );
+    action->setChecked( opt_thumbs );
+    action->setShortcut( Qt::Key_F6 );
+    connect( action, SIGNAL( toggled(bool) ), this, SLOT(slotThumbs()) );
+
+    action = menu->addAction( tr("Show &directories only") );
+    action->setCheckable( true );
+    action->setChecked( opt_dirs_only );
+    //action->setShortcut( Qt::Key_F6 );
+    connect( action, SIGNAL( toggled(bool) ), this, SLOT(slotShowDirsOnly()) );
+
+    /*--------------------------------------------------------------------*/
+
+    menu = menuBar()->addMenu( tr("&Go"));
+
+    menu->addAction( tr("Change &directory"), this, SLOT(slotChangeDir()), '`' );
+    action = menu->addAction( tr("Go to &home directory"), this, SLOT(slotHomeDir()), '~' );
+    action->setIcon( QIcon( ":/images/go-home.png" ) );
+
+    /*--------------------------------------------------------------------*/
+
+    menu = menuBar()->addMenu( tr("&Window"));
+
+    action = menu->addAction( tr("&New window"), this, SLOT(slotNewWindow()), Qt::Key_F3 );
+    action->setIcon( QIcon( ":/images/window-new.png" ) );
+
+    menu->addAction( tr("&Close window"), this, SLOT(close()), Qt::Key_F4 );
+
+
+/*
+    action = menu->addAction(tr("Animated docks"));
+    action->setCheckable(true);
+    action->setChecked(dockOptions() & AnimatedDocks);
+    connect(action, SIGNAL(toggled(bool)), this, SLOT(setDockOptions()));
+
+    action = menu->addAction(tr("Allow nested docks"));
+    action->setCheckable(true);
+    action->setChecked(dockOptions() & AllowNestedDocks);
+    connect(action, SIGNAL(toggled(bool)), this, SLOT(setDockOptions()));
+
+    action = menu->addAction(tr("Allow tabbed docks"));
+    action->setCheckable(true);
+    action->setChecked(dockOptions() & AllowTabbedDocks);
+    connect(action, SIGNAL(toggled(bool)), this, SLOT(setDockOptions()));
+
+    action = menu->addAction(tr("Force tabbed docks"));
+    action->setCheckable(true);
+    action->setChecked(dockOptions() & ForceTabbedDocks);
+    connect(action, SIGNAL(toggled(bool)), this, SLOT(setDockOptions()));
+
+    action = menu->addAction(tr("Vertical tabs"));
+    action->setCheckable(true);
+    action->setChecked(dockOptions() & VerticalTabs);
+    connect(action, SIGNAL(toggled(bool)), this, SLOT(setDockOptions()));
+*/
+}
+
+QAction *addAction(QMenu *menu, const QString &text, QActionGroup *group, QSignalMapper *mapper,
+                    int id)
+{
+    bool first = group->actions().isEmpty();
+    QAction *result = menu->addAction(text);
+    result->setCheckable(true);
+    result->setChecked(first);
+    group->addAction(result);
+    QObject::connect(result, SIGNAL(triggered()), mapper, SLOT(map()));
+    mapper->setMapping(result, id);
+    return result;
+}
+
+
+
+void QvvMainWindow::showEvent(QShowEvent *event)
+{
+    QMainWindow::showEvent(event);
+}
+
+void QvvMainWindow::switchLayoutDirection()
+{
+    if (layoutDirection() == Qt::LeftToRight)
+        qApp->setLayoutDirection(Qt::RightToLeft);
+    else
+        qApp->setLayoutDirection(Qt::LeftToRight);
+}
---
+++ qvv_view.cpp
@@ -0,0 +1,330 @@
+
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QPainter>
+#include <QKeyEvent>
+
+#include <qdebug.h>
+
+#include <qvv.h>
+
+QvvView::QvvView( QvvMainWindow* a_mw )
+{
+  mw         = a_mw;
+  loaded     = 0;
+  mouse_pan  = 0;
+  rotation   = 0;
+  opt_fit    = 1;
+  opt_center = 0;
+  pm         = NULL;
+}
+
+QvvView::~QvvView()
+{
+  close();
+}
+
+void QvvView::moverel( int dx, int dy )
+{
+  move( x() + dx, y() + dy );
+};
+
+void QvvView::slotCenter()
+{
+  if( ! opt_center ) return;
+  // COPYRIGHT :)
+  // taken as is from http://sector.ynet.sk/qt4-tutorial/my-first-qt-gui-application.html
+  QDesktopWidget *desktop = QApplication::desktop();
+
+  int screenWidth, width;
+  int screenHeight, height;
+  int x, y;
+  QSize windowSize;
+
+  screenWidth  = desktop->width(); // get width of screen
+  screenHeight = desktop->height(); // get height of screen
+
+  windowSize = size(); // size of our application window
+  width      = windowSize.width();
+  height     = windowSize.height();
+
+  // little computations
+  x = ( screenWidth  - width  ) / 2;
+  y = ( screenHeight - height ) / 2;
+  y -= 50;
+
+  // move window to desired coordinates
+  move( x, y );
+};
+
+void QvvView::load( QString fn )
+{
+  file_name = fn;
+  reView( opt_fit ? -1 : 100 );
+}
+
+void QvvView::reView( int a_scale )
+{
+  // file_name = ":/images/Half_Face_by_uzorpatorica.jpg";
+
+  QImage im
+  im.load( file_name );
+  pm = new QPixmap( im.width(), im.height() );
+  *pm = QPixmap::fromImage( im );
+  loaded = pm->isNull() ? 0 : 1;
+
+  setWindowTitle( file_name );
+  setWindowIcon( QIcon( *pm ) );
+  slotCenter();
+  move( 0, 0 );
+  resize( pm->width(), pm->height() );
+  setMinimumSize( pm->width(), pm->height() );
+  setMaximumSize( pm->width(), pm->height() );
+  show();
+
+  qDebug() << "REVIEW: [" + file_name + "] loaded:"+QVariant( loaded ).toString() << " visible:" << isVisible() << pm->width() << pm->height();
+
+/*
+  loaded = 1;
+  if ( strcmp( fname, "*logo*" ) == 0 )
+    {
+    img.loadFromData( (const uchar*)&human_dreams_data, sizeof(human_dreams_data) - 1 );
+    }
+  else if (!img.load( fname ))
+    {
+    char caps[1024];
+    sprintf( caps, "LOAD FAILED: %s", fname );
+    setCaption( caps );
+    resize( width(), 100 );
+    setMinimumSize( width(), 100 );
+    setMaximumSize( width(), 100 );
+    show();
+    loaded = 0;
+    return;
+    }
+  // fprintf( stderr, "qvv: load image: %s, %s\n", fname, loaded ? "ok" : "fail" );
+
+  int w = img.width();
+  int h = img.height();
+
+  scale = a_scale;
+
+  if ( scale == -1 )
+    {
+    if ( w <= DeskW && h <= DeskH )
+      scale = 100;
+    else
+      scale = MIN( ((100 * DeskW) / w) , ((100 * DeskH) / h) );
+    }
+  else if ( scale == -2 )
+    {
+    w = app->desktop()->width();
+    h = app->desktop()->height();
+    scale = 100;
+    }
+  else
+    {
+    scale = (scale / 10) * 10;
+    }
+
+  if ( scale < MINSCALE ) scale = MINSCALE;
+  if ( scale > MAXSCALE ) scale = MAXSCALE;
+
+  w = (w * scale) / 100;
+  h = (h * scale) / 100;
+
+  if (w > DeskW || h > DeskH)
+  {
+      statusMsg("Invert Move");
+      invertMove = -1;
+  }
+  else
+  {
+      statusMsg("Normal Move");
+      invertMove = 1;
+  }
+
+  if ( scale != 100 )
+    pm.fromImage( img.scaled( QSize( w, h ) ) );
+  else if ( w != img.width() || h != img.height() )
+    pm.fromImage( img.scaled( QSize( w, h ) ) );
+  else
+    pm.fromImage( img );
+
+
+  if ( rotation )
+    {
+    QWMatrix m;
+    m.rotate( double(rotation) );
+    pm = pm.xForm(m);
+
+    w = pm.width();
+    h = pm.height();
+    }
+
+  resize( w, h );
+  setMinimumSize( w, h );
+  setMaximumSize( w, h );
+  show();
+  if ( optCenter )
+    CenterView( this );
+  else
+    move( ViewX, ViewY );
+
+  char caps[1024];
+  sprintf( caps, "%dx%d@%d%% %s", img.width(), img.height(), scale, fname );
+  setCaption( caps );
+  update();
+
+  // fprintf( stderr, "scale set to: %d%%\n", scale );
+*/
+};
+
+void QvvView::paintEvent( QPaintEvent * pe )
+{
+  if( ! loaded ) return;
+  QPainter painter( this );
+  painter.drawPixmap( 0, 0, *pm );
+};
+
+void QvvView::keyPressEvent( QKeyEvent * e )
+{
+  if( e->modifiers() & Qt::ALT )
+    {
+    switch( e->key() )
+      {
+/*
+      case Qt::Key_X   : QApplication::quit(); break;
+*/
+      default: QWidget::keyPressEvent( e );
+      }
+    }
+  else
+    {
+    switch( e->key() )
+      {
+      case Qt::Key_Home  : move( 0, 0 ); break;
+
+      case Qt::Key_Left         : moverel( -16, 0 ); break;
+      case Qt::Key_Right        : moverel( +16, 0 ); break;
+      case Qt::Key_Up           : moverel( 0, -16 ); break;
+      case Qt::Key_Down         : moverel( 0, +16 ); break;
+
+      case Qt::Key_Escape:
+      case Qt::Key_Return:
+      case Qt::Key_F4    : mw->closeView( this );
+                           break;
+
+      case Qt::Key_Insert       : opt_center = ! opt_center;
+      case Qt::Key_Tab          : slotCenter(); moverel( 0, 0 ); break;
+
+      case Qt::Key_PageUp       :
+      case Qt::Key_BracketLeft  : if (mw) mw->slotGoPrev(); break;
+      case Qt::Key_Space        :
+      case Qt::Key_PageDown     :
+      case Qt::Key_BracketRight : if (mw) mw->slotGoNext(); break;
+
+/*
+      case Qt::Key_F3    : slotNewWindow(); break;
+      case Qt::Key_Left  : slotGoUp(); break;
+      case Qt::Key_Right :
+      case Qt::Key_Return: Enter( tree->currentItem() ); break;
+
+      //case Qt::Key_F6    : slotThumbs(); break;
+
+
+      case Qt::Key_F1    : closeAll();
+                           views.append( new qvvView( this ) );
+                           views.first()->load( "*logo*" );
+                           break;
+
+      case Qt::Key_F5    : loadDir( cdir.absolutePath() ); break;
+
+      case Qt::Key_BracketLeft  : slotGoPrev(); break;
+      case Qt::Key_BracketRight : slotGoNext(); break;
+      case Qt::Key_Backspace    :
+      case Qt::Key_Escape       : closeAll(); break;
+      case Qt::Key_Insert       : optCenter = !optCenter; break;
+      case Qt::Key_Delete       : slotDelete(); break;
+*/
+/*
+      default:
+        if ( e->text().toAscii().at(0) && isalnum(e->text().toAscii().at(0)) )
+          find( e->text().toAscii().at(0) );
+        else
+          switch( e->text().toAscii().at(0) )
+            {
+            case 13  : Enter( tree->currentItem() ); break;
+            case '~' : goToDir( '~' ); break;
+            case '`' : goToDir( '`' ); break;
+            default: QMainWindow::keyPressEvent( e );
+            }
+========================================================
+    switch( e->key() )
+      {
+      case ALT+Qt::Key_X        : app->quit(); break;
+      case Qt::Key_Home         : move(  0,  0 ); break;
+
+      case Qt::Key_Left         : moverel( -16*invertMove, 0 ); break;
+      case Qt::Key_Right        : moverel( +16*invertMove, 0 ); break;
+      case Qt::Key_Up           : moverel( 0, -16*invertMove ); break;
+      case Qt::Key_Down         : moverel( 0, +16*invertMove ); break;
+
+      case Qt::Key_Insert       : optCenter = !optCenter;
+      case Qt::Key_Tab          : CenterView( this ); moverel( 0, 0 ); break;
+
+      case Qt::Key_F1           : load( "*logo*" ); break;
+      case Qt::Key_F4           :
+      case Qt::Key_Escape       : if (mw) mw->views.removeOne( this );
+                              close(); delete this; break;
+
+      case Qt::Key_F5           : reView( scale ); break;
+
+      case Qt::Key_PageUp       :
+      case Qt::Key_BracketLeft  : if (mw) mw->slotGoPrev(); break;
+      case Qt::Key_Space        :
+      case Qt::Key_PageDown     :
+      case Qt::Key_BracketRight : if (mw) mw->slotGoNext(); break;
+      case Qt::Key_Delete       : if (mw)
+                                {
+                                mw->slotDelete();
+                                mw->Enter( mw->vb->currentItem() );
+                                }
+                                break;
+      default:
+              switch( e->text().toAscii().at(0) )
+              {
+              case 13   : if (mw) mw->views.removeOne( this );
+                          close(); delete this; break;
+              case '+'  : reView( scale + 20 ); break;
+              case '-'  : reView( scale - 20 ); break;
+              case '*'  : reView(  -1 ); optFit = 1; break;
+              case '/'  : reView( 100 ); optFit = 0; break;
+
+              case '1'  : reView( 100 ); break;
+              case '2'  : reView( 200 ); break;
+              case '3'  : reView( 300 ); break;
+              case '4'  : reView( 400 ); break;
+              case '5'  : reView( 500 ); break;
+
+        case '<'  :
+        case ','  : rotation -= 90; reView( scale ); break;
+        case '>'  :
+        case '.'  : rotation += 90; reView( scale ); break;
+
+              case 'Z'  : reView( -2 );
+              case 'z'  : setDesktopBackground(); break;
+
+              default:
+                QWidget::keyPressEvent( e );
+              break;
+              }
+      break;
+      }
+======================================================
+
+*/
+      default: QWidget::keyPressEvent( e );
+      }
+    }
+};
